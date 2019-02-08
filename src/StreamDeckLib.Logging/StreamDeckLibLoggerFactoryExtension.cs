58d369cad678803812ca19ed9a1cf9679c1d3834
Lee Williams <lee.williams@topcashback.co.uk>
2019-02-08 18:07:24 +0000

    create separate logging library
    Why
    ===
    This allows us to provide some logging for plugins to use should their
    authors wish to use it.

---
+++ src/StreamDeckLib.Logging/StreamDeckLibLoggerFactoryExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace StreamDeckLib.Logging
+{
+    public static class StreamDeckLibLoggerFactoryExtension
+		{
+			public static ILoggerFactory AddDefaultLogging(this ILoggerFactory loggerFactory)
+			{
+				if (loggerFactory == null)
+				{
+					loggerFactory = new LoggerFactory();
+				}
+
+				loggerFactory.AddSerilog(Log.Logger);
+
+				return loggerFactory;
+		}
+  }
+}
